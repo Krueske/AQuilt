29c3d747fc70eda1c5ba0d4bf49d42d6188ce0dd
Krueske <97344273+Krueske@users.noreply.github.com>
2025-05-15 21:00:52 +0800

    Update ceval_sft_dataGen.py

--- dataGen/ceval_sft_dataGen.py
+++ dataGen/ceval_sft_dataGen.py
@@ -1,206 +1,145 @@
 import os
 import sys
 import json
-# os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-import torch
-import numpy as np
+import re
 import random
 import pandas as pd
-from vllm import LLM, SamplingParams
-import pyarrow.parquet as pq
 from tqdm import tqdm
+import numpy as np
+import torch
+from transformers import LlamaTokenizer, AutoConfig, LlamaForCausalLM, GenerationConfig
+# vllm 相关导入
+from vllm import LLM, SamplingParams
+from vllm.lora.request import LoRARequest
 def setup_seed(seed):
-    random.seed(seed)   # Python的随机性
-    os.environ['PYTHONHASHSEED'] = str(seed)    # 设置Python哈希种子，为了禁止hash随机化，使得实验可复现
-    np.random.seed(seed)   # numpy的随机性
-    torch.manual_seed(seed)   # torch的CPU随机性，为CPU设置随机种子
-    torch.cuda.manual_seed(seed)   # torch的GPU随机性，为当前GPU设置随机种子
-    torch.cuda.manual_seed_all(seed)  # if you are using multi-GPU.   torch的GPU随机性，为所有GPU设置随机种子
-    # torch.backends.cudnn.benchmark = False   # if benchmark=True, deterministic will be False
-    # torch.backends.cudnn.deterministic = True   # 选择确定性算法
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
 
 setup_seed(0)
-task_prompt_dict ={
-"单项选择问答":"""请你从提供的参考资料中生成一个单项选择题帮助学生更好地掌握相关知识：
-其中单项选择题题目应该包括一个问题（question），四个选择项（options）标记位A、B、C和D，其中一个是问题的答案(answer);
-同时你还需要生成解答问题的思考步骤(thinking_steps)，以及这个问题的答案(answer)。
-并按照以下json格式输出
-```json
-{"question": "xxx", "thinking_steps": "xxx", "answer": "xxx"}
-```
-""",
-"多项选择问答":"""请你从提供的参考资料中生成一个多项选择题帮助学生更好地掌握相关知识：
-其中多项选择题题目应该包括一个问题（question），多个选择项（options）标记位A、B、C、D、E（以此类推），其中一个或多个是问题的答案(answer);
-同时你还需要生成解答问题的思考步骤(thinking_steps)，以及这个问题的答案(answer)。
-并按照以下json格式输出
-```json
-{"question": "xxx", "thinking_steps": "xxx", "answer": "xxx"}
-```
-""",
-"闭卷问答":"""请你从提供的参考资料中生成一个不需要参考文本回答的闭卷问答对帮助学生更好地掌握相关知识：
-这个问答对应该包括一个问题（question），同时你还需要生成解答问题的思考步骤(thinking_steps)，以及这个问题的答案(answer)。
-并按照以下json格式输出
-```json
-{"question": "xxx", "thinking_steps": "xxx", "answer": "xxx"}
-```
-""",
-"开卷问答":"""请你从提供的参考资料中生成一个可以参考用户提供的参考资料进行回答的开卷问答对帮助学生更好地掌握相关知识：
-这个问答对应该包括一个问题（question），同时你还需要生成解答问题的思考步骤(thinking_steps)，以及这个问题的答案(answer)。
-并按照以下json格式输出
-```json
-{"question": "xxx", "thinking_steps": "xxx", "answer": "xxx"}
-```
-""",
-"single choice question answering": """Please generate a single-choice question from the provided reference materials to help students better grasp the relevant knowledge:
-The single-choice question should include a question, four options labeled A, B, C, and D, one of which is the answer to the question;
-At the same time, you also need to generate the thinking steps for solving the question, as well as the answer to this question.
-And output in the following JSON format:
-```json
-{"question": "xxx", "thinking_steps": "xxx", "answer": "xxx"}
-```
-""",
-"multi choice question answering": """Please generate a multiple-choice question from the references provided to help students better grasp the knowledge:
-The multiple-choice question should include a question with multiple options tags A, B, C, D, E (and so on), one or more of which are the answers to the questions;
-At the same time, you also need to generate the thinking steps for solving the question, as well as the answer to this question.
-And output in the following JSON format:
-```json
-{"question": "xxx", "thinking_steps": "xxx", "answer": "xxx"}
-```
-""",
-"close question answering": """Please generate a closed-book question and answer pair from the provided reference materials that do not require reference text to answer to help students better grasp the relevant knowledge:
-This Q&A pair should include a question, and you also need to generate the thinking steps for solving the question, as well as the answer to this question.
-And output in the following JSON format:
-```json
-{"question": "xxx", "thinking_steps": "xxx", "answer": "xxx"}
-```
-""",
-"open question answering": """Please generate an open-book Q&A pair from the provided reference materials to help students better grasp the relevant knowledge:
-This Q&A pair should include a question, and you also need to generate the thinking steps for solving the question, as well as the answer to this question.
-And output in the following JSON format:
-```json
-{"question": "xxx", "thinking_steps": "xxx", "answer": "xxx"}
-```
-""",
-"data complexity evaluation": """Please score the complexity of the user's instruction to help students understand the complexity of the questions.
-There are 5 levels of complexity, which are: 1 point (Simple Question), 2 points (Basic Question), 3 points (Moderate Complexity Question), 4 points (Higher Complexity Question), 
-You'll first need to analyze the complexity of the question before grading it.
-And output in the following JSON format:
-```json
-{"analysis_steps": "xxx", "score": "xxx"}
-```
-""",
-"data quality evaluation": """Please score the quality of the user's instruction and response to help students understand the quality of the question and response.
-There are 5 levels of quality, which are: 1 point - Basic requirements met (Basic Level), 2 points - Basic requirements met with some quality (Qualified Level), \
-3 points - Good quality, meeting most requirements (Good Level), 4 points - High quality, meeting all requirements and exceeding expectations (Excellent Level), \
-5 points - Excellent quality, exceeding all requirements with professional contributions (Outstanding Level)
-You'll first need to analyze the quality of the question and response before grading it.
-And output in the following JSON format:
-```json
-{"analysis_steps": "xxx", "score": "xxx"}
-```
-""",
-"text summarization": """Please generate a concise summary Q&A pairs of the provided text to help students better understand the main points:
-The summary should capture the key ideas and essential information from the text.
-The content you generate should include a question, and you also need to generate the thinking steps for solving the question, as well as the answer to this question.
-And output in the following JSON format:
-```json
-{"question": "xxx", "thinking_steps": "xxx", "answer": "xxx"}
-```
-""",
-"text generation": """Please generate a text-generated Q&A pair based on the text provided to help students learn:
-The resulting text should be well-structured and relevant to the given text.
-The content you generate should include a question, and you also need to generate the thinking steps for solving the question, as well as the answer to this question.
-And output in the following JSON format:
-```json
-{"question": "xxx", "thinking_steps": "xxx", "answer": "xxx"}
-```
-""",
-"natural language inference": """Please generate a logical inference question from the provided reference materials to help students better grasp the relevant knowledge:
-Logical inference questions generally ask whether a judgment or piece of knowledge is correct, with answers including "yes, no, maybe" three options.
-The content you generate should include a question, and you also need to generate the thinking steps for solving the question, as well as the answer to this question.
-And output in the following JSON format:
-```json
-{"question": "xxx", "thinking_steps": "xxx", "answer": "xxx"}
-```
-""",
-"text classification": """Generate a text classification task based on the text provided to help students understand the content of the text:
-Classifications should be accurate and relevant to the given text.
-The content you generate should include a question, and you also need to generate the thinking steps for solving the question, as well as the answer to this question.
-And output in the following JSON format:
-```json
-{"question": "xxx", "thinking_steps": "xxx", "answer": "xxx"}
-```
-""",
-"文本摘要": """请根据提供的文本生成一个合理的摘要问答对，摘要应捕捉文本中的关键思想和基本信息，以帮助学生更好地理解主要观点：
-你生成的内容应该包括一个问题，同时你还需要生成解答问题的思考步骤，以及这个问题的答案。
-并按照以下json格式输出
-```json
-{"question": "xxx", "thinking_steps": "xxx", "answer": "xxx"}
-```
-""",
-"文本生成": """请根据提供的文本生成一个文本生成问答对，以帮助学生学习相关知识并增强文本生成能力：
-你生成的内容应该包括一个问题，同时你还需要生成解答问题的思考步骤，以及这个问题的答案。
-并按照以下json格式输出
-```json
-{"question": "xxx", "thinking_steps": "xxx", "answer": "xxx"}
-```
-""",
-"自然语言推断": """请根据提供的参考资料生成一个逻辑推断题，以帮助学生更好地掌握相关知识：
-你生成的内容应该包括一个问题，同时你还需要生成解答问题的思考步骤，以及这个问题的答案。
-并按照以下json格式输出
-```json
-{"question": "xxx", "thinking_steps": "xxx", "answer": "xxx"}
-```
-""",
-"文本分类": """请根据提供的文本生成一个文本分类任务，以帮助学生理解文本的内容与类别：
-分类应准确且与给定的文本相关。
-你生成的内容应该包括一个问题，同时你还需要生成解答问题的思考步骤，以及这个问题的答案。
-并按照以下json格式输出
-```json
-{"question": "xxx", "thinking_steps": "xxx", "answer": "xxx"}
-```
-"""
-}
-zh_prompt_template = """以下是给定的参考资料：
-[参考资料开始]
-{text}
-[参考资料结束]
-请根据要求完成问答对生成。
-"""
-en_prompt_template = """Here is the provided reference material:
-[reference material begin]
-{text}
-[reference material end]
-Please complete the Q&A pair based on the requirements.
-"""
+
+
+
+# 加载 vllm 模型
 model_path = sys.argv[1]
-file_path = sys.argv[2]
-output_data_path = sys.argv[3]
-llm = LLM(model=model_path,tensor_parallel_size=1,max_model_len=8192)
-sampling_params = SamplingParams(temperature=0.7, top_p=0.95, max_tokens=1024)
+llm = LLM(model=model_path, tensor_parallel_size=1, max_model_len=8192, gpu_memory_utilization=0.9)
+sampling_params = SamplingParams(temperature=0, max_tokens=1024)
+def extract_answer(line, gen_ans):
+    choices = ["A", "B", "C", "D"]
+    m = re.findall(r'所以答案是(.+?)。', gen_ans, re.M)
+    if len(m) > 0 and m[-1] in choices:
+        return m[-1], True
+    answer_patterns = [
+        r'([ABCD])是正确的',
+        r'选项([ABCD])正确',
+        r'答案为([ABCD])',
+        r'答案是([ABCD])',
+        r'答案([ABCD])',
+        r'选择([ABCD])',
+        r'答案：([ABCD])',
+        r'答案是：([ABCD])',
+        r'选择答案([ABCD])',
+        r'答案应该是([ABCD])',
+        r'答案是选项([ABCD])',
+        r'答案应该是选项([ABCD])',
+        r'答案为选项([ABCD])',
+        r'answer is ([ABCD])',
+        r'answer: ([ABCD])',
+        r'So option ([ABCD])',
+        r'So, option ([ABCD])',
+        r'answer is:\n\n([ABCD])',
+        r'Therefore, option ([ABCD])',
+        r'答案是 ([ABCD])'
+    ]
+    for answer_pattern in answer_patterns:
+        m = re.search(answer_pattern, gen_ans, re.M)
+        if m:
+            answer = m.group(1)
+            return answer, False
+    m = re.findall(r'[ABCD]', gen_ans, re.M)
+    if len(m) >= 1:
+        answer = m[0]
+        return answer, False
+    choices_dict = {}
+    pattern = ""
+    for c in choices:
+        choices_dict[str(line[f'{c}'])] = c
+        pattern += re.escape(str(line[f'{c}'])) + "|"
+    pattern = pattern[:-1]
+    m = re.findall(pattern, gen_ans, re.M)
+    if len(m) >= 1:
+        answer = choices_dict[m[0]]
+        return answer, False
+    return random.choice('ABCD'), False
 
+def files_name(directory):
+    files_and_folders = os.listdir(directory)
+    # 过滤出文件名，假设你只想要文件名，不包括文件夹
+    file_names = [file for file in files_and_folders if os.path.isfile(os.path.join(directory, file))]
+    # 文件名列表
+    subjects = []
+    for file_name in file_names:
+        subject = re.sub('_test.csv', '', str(file_name))
+        subjects.append(subject)
+    return subjects
 
-with open(file_path, 'r', encoding='utf-8') as f:
-    data = json.load(f)
-results = []
-texts_with_prompts = []
-for i in range(len(data)):
-    texts_with_prompts.append(task_prompt_dict["单项选择问答"] + zh_prompt_template.format(text=data[i]["article"]))
-texts_with_prompts = texts_with_prompts*2000
-texts_with_prompts = texts_with_prompts[0:20000]
-num_gens = 1
-for num_gen in range(num_gens):
-    outputs = llm.generate(texts_with_prompts, sampling_params)
-    num = -1
+def test(subject_name):
+    df = pd.read_csv("./CEVAL/test/{}_test.csv".format(subject_name), header=0)
+    lines = df.to_dict(orient='records')
+    in_context = ""
+    df_dev = pd.read_csv("./CEVAL/dev/{}_dev.csv".format(subject_name), header=0)
+    for i in range(len(df_dev)):
+        in_context += "{question}\nA. {A}\nB. {B}\nC. {C}\nD. {D}\n答案是什么？{answer}\n".format(
+            question=df_dev.loc[i, 'question'], A=df_dev.loc[i, 'A'], B=df_dev.loc[i, 'B'], C=df_dev.loc[i, 'C'],
+            D=df_dev.loc[i, 'D'], answer=df_dev.loc[i, 'answer'])
+    model_type = "instruct"
+    if model_type == "instruct":
+        in_context = ""
+        data_with_prompts = []
+        for i in range(len(df)):
+            if "llama" in model_path:
+                data_with_prompts.append(
+                    """<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\n<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n以下是中国关于{subject}考试的单项选择题，请选出其中的正确答案。\n\n{in_context}{question}\nA. {A}\nB. {B}\nC. {C}\nD. {D}\n答案是什么？<|eot_id|><|start_header_id|>assistant<|end_header_id|>""".format(
+                        subject=subject_name, in_context=in_context, question=df.loc[i, 'question'], A=df.loc[i, 'A'],
+                        B=df.loc[i, 'B'], C=df.loc[i, 'C'], D=df.loc[i, 'D']))
+            elif "qwen" in model_path:
+                data_with_prompts.append(
+                    """<|im_start|>system\nYou are a helpful assistant.<|im_end|>\n<|im_start|>user\n以下是中国关于{subject}考试的单项选择题，请选出其中的正确答案。\n\n{in_context}{question}\nA. {A}\nB. {B}\nC. {C}\nD. {D}\n答案是什么？<|im_end|>\n<|im_start|>assistant\n""".format(
+                        subject=subject_name, in_context=in_context, question=df.loc[i, 'question'], A=df.loc[i, 'A'],
+                        B=df.loc[i, 'B'], C=df.loc[i, 'C'], D=df.loc[i, 'D']))
+    elif model_type == "base":
+        data_with_prompts = []
+        for i in range(len(df)):
+            data_with_prompts.append(
+            """以下是中国关于{subject}考试的单项选择题，请选出其中的正确答案。\n\n{in_context}{question}\nA. {A}\nB. {B}\nC. {C}\nD. {D}\n答案是什么？""".format(
+                subject=subject_name,in_context=in_context,question=df.loc[i, 'question'], A=df.loc[i, 'A'], B=df.loc[i, 'B'], C=df.loc[i, 'C'], D=df.loc[i, 'D']))
+    
+    outputs = llm.generate(data_with_prompts, sampling_params=sampling_params)
+    subject_res = {}
     for idx in tqdm(range(len(outputs))):
-        num+=1
         output = outputs[idx]
         generated_text = output.outputs[0].text
-        try:
-            qa_pair = json.loads(generated_text.replace("```json","").replace("```","").strip())
-        except:
-            continue
-        data[num].update(qa_pair)
-        results.append({"context":data[num]["article"],"qa_pair":qa_pair})
-with open(output_data_path, "w", encoding='utf-8') as f:
-    json.dump(results, f, ensure_ascii=False, indent=4)
+        generated_text1 = generated_text.split("Answer")[-1]
+        ans, direct_extract = extract_answer(lines[idx], generated_text1)
+        subject_res[str(idx)] = ans
+        if idx == 0:
+            print(generated_text)
+            print(ans)
+            print("--------------------------------------------------")
+    return subject_res
+
+subjects = files_name('./CEVAL/test')
+results = {}
+for i in range(len(subjects)):
+    subject_name = subjects[i]
+    result1 = test(subject_name)
+    results[subject_name] = result1
+model_name = model_path.split("/")[-1]
+with open(f"./CEVAL/results/{model_name}.json", "w", encoding='utf-8') as f:
+    json.dump(results, f, indent=4, ensure_ascii=False)
+    print("测试完成...")
