7aac4f0f3f1462d080d86d5164e1928937942d87
Krueske <97344273+Krueske@users.noreply.github.com>
2025-07-03 14:58:49 +0800

    Update dataEval.sh

--- dataEval.sh
+++ dataEval.sh
@@ -1,4 +1,5 @@
 model_path=""
+eval_lora_path=""
 data_path=""
 output_datapath=""
-CUDA_VISIBLE_DEVICES=0 python -u data_eval.py $model_path $data_path > ./logs/data_eval.log 2>&1 &
+CUDA_VISIBLE_DEVICES=0 python -u data_eval.py $model_path $eval_lora_path $data_path $output_datapath > ./logs/data_eval.log 2>&1 &
