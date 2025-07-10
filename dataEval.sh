64644e71600939a908b38f6523fd0048d7971388
Krueske <97344273+Krueske@users.noreply.github.com>
2025-07-10 17:07:17 +0800

    Delete dataEval.sh

--- dataEval.sh
+++
@@ -1,5 +0,0 @@
-model_path=""
-eval_lora_path=""
-data_path=""
-output_datapath=""
-CUDA_VISIBLE_DEVICES=0 python -u data_eval.py $model_path $eval_lora_path $data_path $output_datapath > ./logs/data_eval.log 2>&1 &
