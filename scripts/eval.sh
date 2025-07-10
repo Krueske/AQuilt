77a269be11c1f82460d76f8049c2b8de39104b73
Krueske <97344273+Krueske@users.noreply.github.com>
2025-07-10 17:07:59 +0800

    Update eval.sh

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,3 +1,3 @@
 model_path=""
 domain_task=""
-CUDA_VISIBLE_DEVICES=0 python -u ./tests/$domain_task\_test.py $model_path > ./logs/$domain_task\_eval.log 2>&1 &
+CUDA_VISIBLE_DEVICES=0 python -u ../tests/$domain_task\_test.py $model_path > ./logs/$domain_task\_eval.log 2>&1 &
