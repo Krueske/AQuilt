45d8f877a40db17be317b35abf2118534f20f3b8
Krueske <97344273+Krueske@users.noreply.github.com>
2025-07-10 17:07:47 +0800

    Update dataGen.sh

--- scripts/dataGen.sh
+++ scripts/dataGen.sh
@@ -2,4 +2,4 @@ domain_task=""
 model_path=""
 unlabeled_data_path=""
 output_data_path=""
-CUDA_VISIBLE_DEVICES=0 python -u ./dataGen/$domain_task\_sft_dataGen.py $model_path $unlabeled_data_path $outoput_data_path > ./logs/data_gen.log 2>&1 &
+CUDA_VISIBLE_DEVICES=0 python -u ../dataGen/$domain_task\_sft_dataGen.py $model_path $unlabeled_data_path $outoput_data_path > ./logs/data_gen.log 2>&1 &
